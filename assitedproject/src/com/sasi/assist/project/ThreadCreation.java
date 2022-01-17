a46db83f7fbb559e7b1677e41612de57e35a7c72
ELCOT <ELCOT@Lenovo>
2022-01-17 09:53:34 +0530

    commit 10

---
+++ assitedproject/src/com/sasi/assist/project/ThreadCreation.java
@@ -0,0 +1,25 @@
+package com.sasi.assist.project;
+
+public class ThreadCreation extends Thread {
+	public static void main( String args[] ){
+  		ThreadCreation mt = new ThreadCreation();
+  		mt.start();
+  		
+//  		new ThreadCreation().run();
+	}
+	
+	
+	
+
+	
+	 	public void run(){
+	  		System.out.println("concurrent thread started running..");
+	  		
+	  		
+	  		
+	}
+	 
+	 	
+	}
+
+
